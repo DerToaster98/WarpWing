6a6e1450e9e59a2309afb37647f34dc0483f5d4d
DerToaster98 <38782719+DerToaster98@users.noreply.github.com>
2023-08-06 16:17:55 +0200

    begin implementing burnt and burning wing mechanics

--- src/main/java/de/dertoaster/warpwing/EventHandlerMod.java
+++ src/main/java/de/dertoaster/warpwing/EventHandlerMod.java
@@ -1,6 +1,8 @@
 package de.dertoaster.warpwing;
 
 import de.dertoaster.warpwing.init.WWItems;
+import de.dertoaster.warpwing.item.ItemWarpWing;
+import de.dertoaster.warpwing.item.ItemWarpWing.EWingState;
 import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
@@ -14,6 +16,8 @@ public class EventHandlerMod {
 	public static void assignItemsToTabs(BuildCreativeModeTabContentsEvent event) {
 		if (event.getTabKey().equals(CreativeModeTabs.TOOLS_AND_UTILITIES)) {
 			event.accept(WWItems.ITEM_WARP_WING.get());
+			event.accept(ItemWarpWing.setState(WWItems.ITEM_WARP_WING.get().getDefaultInstance(), EWingState.BURNING));
+			event.accept(ItemWarpWing.setState(WWItems.ITEM_WARP_WING.get().getDefaultInstance(), EWingState.BURNT));
 		}
 	}
 	
--- src/main/java/de/dertoaster/warpwing/item/ItemWarpWing.java
+++ src/main/java/de/dertoaster/warpwing/item/ItemWarpWing.java
@@ -14,6 +14,7 @@ import net.minecraft.sounds.SoundEvent;
 import net.minecraft.sounds.SoundSource;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResultHolder;
+import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
@@ -71,9 +72,16 @@ public class ItemWarpWing extends ItemLore {
 			}
 			ServerLevel respawnDimension = world.getServer().getLevel(player.getRespawnDimension());
 			if (respawnPosition != null) {
-				item.hurtAndBreak(1, user, (p_220038_0_) -> {
-					p_220038_0_.broadcastBreakEvent(EquipmentSlot.MAINHAND);
-				});
+				// Burnt wing is always a one-time-use item
+				if (getState(item).equals(EWingState.BURNT)) {
+					item.hurtAndBreak(item.getMaxDamage() - item.getDamageValue(), user, (p_220038_0_) -> {
+						p_220038_0_.broadcastBreakEvent(EquipmentSlot.MAINHAND);
+					});
+				} else {
+					item.hurtAndBreak(1, user, (p_220038_0_) -> {
+						p_220038_0_.broadcastBreakEvent(EquipmentSlot.MAINHAND);
+					});
+				}
 				player.teleportTo(respawnDimension, respawnPosition.getX(), respawnPosition.getY(), respawnPosition.getZ(), player.getRespawnAngle(), 0);
 				player.sendSystemMessage(Component.translatable("item." + this.getRegistryName().getNamespace() + "." + this.getRegistryName().getPath() + ".teleporting"));
 				final Holder<SoundEvent> soundHolder = Holder.direct(WWSounds.ITEM_WARP_WING_WOOSH.get());
@@ -85,22 +93,62 @@ public class ItemWarpWing extends ItemLore {
 		
 		return item;
 	}
-
+	
 	public InteractionResultHolder<ItemStack> use(Level world, Player player, InteractionHand hand) {
 		ItemStack itemstack = player.getItemInHand(hand);
 		player.startUsingItem(hand);
 		return InteractionResultHolder.consume(itemstack);
 	}
 	
+	@Override
+	public void inventoryTick(ItemStack pStack, Level pLevel, Entity pEntity, int pSlotId, boolean pIsSelected) {
+		super.inventoryTick(pStack, pLevel, pEntity, pSlotId, pIsSelected);
+		
+		switch (getState(pStack)) {
+		case BURNING:
+			if (pEntity.isInWater() || (pEntity.wasOnFire && pEntity.isOnFire())) {
+				extinguishWing(pStack, pLevel, pEntity);
+			} else {
+				if (pEntity.tickCount % 5 == 0) {
+					if (!(pEntity instanceof Player p && (p.isCreative() || p.isSpectator()))) {
+						pEntity.setSecondsOnFire(5);
+					}
+					pEntity.hurt(pLevel.damageSources().onFire(), 1);
+					if (pEntity.tickCount % 20 == 0) {
+						if (pEntity instanceof LivingEntity le) {
+							pStack.hurtAndBreak(1, le, (arg0) -> {
+								// Send message and play sound
+							});
+						}
+					}
+				}
+			}
+			break;
+		case NORMAL:
+			// If in a hot dimension, BURN!!!
+			break;
+		default:
+			break;
+		
+		}
+	}
+	
+	protected void extinguishWing(ItemStack pStack, Level pLevel, Entity pEntity) {
+		setState(pStack, EWingState.BURNT);
+		pStack.setDamageValue(pStack.getMaxDamage() - 1);
+		// TODO: Play some extignuishing sounds
+	}
+
 	protected static final String cWING_STATE_KEY = "WarpWingWingState";
 	
-	public static void setState(ItemStack stack, EWingState state) {
+	public static ItemStack setState(ItemStack stack, EWingState state) {
 		if (stack.getItem() instanceof ItemWarpWing) {
 			if (getState(stack) != state) {
 				CompoundTag tag = stack.getOrCreateTag();
 				tag.putInt(cWING_STATE_KEY, state.ordinal());
 			}
 		}
+		return stack;
 	}
 
 	public static EWingState getState(ItemStack itemStack) {
