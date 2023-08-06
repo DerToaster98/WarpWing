55cbd37a6c94f2365e883543c9641c0da37f97c9
DerToaster98 <38782719+DerToaster98@users.noreply.github.com>
2023-08-06 16:28:19 +0200

    config values for affecting dimensions

--- src/main/java/de/dertoaster/warpwing/config/WarpWingModConfigHolder.java
+++ src/main/java/de/dertoaster/warpwing/config/WarpWingModConfigHolder.java
@@ -1,12 +1,15 @@
 package de.dertoaster.warpwing.config;
 
 import java.io.File;
+import java.util.List;
 
 import org.apache.commons.lang3.tuple.Pair;
 
 import com.electronwill.nightconfig.core.file.CommentedFileConfig;
 import com.electronwill.nightconfig.core.io.WritingMode;
 
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.level.dimension.BuiltinDimensionTypes;
 import net.minecraftforge.common.ForgeConfigSpec;
 import net.minecraftforge.common.ForgeConfigSpec.ConfigValue;
 
@@ -15,21 +18,34 @@ public class WarpWingModConfigHolder {
 	public static class ItemConfig {
 		private static final int defaultDurability = 16;
 		private static final int defaultUseDuration = 40;
+		private static final List<ResourceLocation> defaultHotDimensions = List.of(BuiltinDimensionTypes.NETHER_EFFECTS);
+		private static final List<ResourceLocation> defaultEnderDimensions = List.of(BuiltinDimensionTypes.END_EFFECTS);
 		
 		public final ConfigValue<Integer> wwDurability;
 		public final ConfigValue<Integer> wwUseDuration;
+		public final ConfigValue<List<ResourceLocation>> wwHotDimensions;
+		public final ConfigValue<List<ResourceLocation>> wwEnderDimensions;
 		
 		public ItemConfig(ForgeConfigSpec.Builder builder) {
 			builder.push("warpwing-item");
 			this.wwUseDuration = builder
 					.comment("Time in ticks it takes to charge up the wing before using it")
 					.worldRestart()
-					.defineInRange("Value must be at least 1", defaultUseDuration, 1, Integer.MAX_VALUE);
+					.defineInRange("useDuration", defaultUseDuration, 1, Integer.MAX_VALUE);
 			
 			this.wwDurability = builder
 					.comment("Durability of the warp wing item")
 					.worldRestart()
-					.defineInRange("Uses of the warp wing, muse be at least 0", defaultDurability, 1, Integer.MAX_VALUE);
+					.defineInRange("durability", defaultDurability, 1, Integer.MAX_VALUE);
+			
+			this.wwHotDimensions = builder
+					.comment("Dimensions in which the wing will burn")
+					.define("hotDimensions", defaultHotDimensions);
+			
+			this.wwEnderDimensions = builder
+					.comment("Dimensions in which the wing will teleport to a random place")
+					.define("enderDimensions", defaultEnderDimensions);
+			
 			builder.pop();
 		}
 	}
